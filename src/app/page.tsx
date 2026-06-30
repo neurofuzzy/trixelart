239dfbbefeb43dcbd11d2ebd50c347ece5bd1f1c
Geoff Gaudreault <gaudreault@gmail.com>
2026-06-30 05:42:32 +0000

    fuck

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import SymmetriaGrid from "@/components/SymmetriaGrid";
-
-export default function Home() {
-  return (
-    <main className="fixed inset-0 w-screen h-screen flex flex-col overflow-hidden bg-black">
-      <SymmetriaGrid />
-    </main>
-  );
-}
